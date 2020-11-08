d3b865025714d7938c609af29a9e4bcaf42409fd
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-11-08 02:03:47 -0500

    Delete done

--- delete.php
+++ delete.php
@@ -1,9 +1,16 @@
 <?php
 require_once 'inc/db.php';
 
-if(isset($_GET['id'])):
+if(isset($_GET['id']) &&_GET['id'] != ''):
     // delete the item
+    $id = $_GET['id'];
+    $sql = "DELETE FROM `corvettes` WHERE `corvettes`.`Vette_id` = {$id}";
+    $result = $pdo->query($sql);
+    header("Location: index.php");
+else:
+    // redirect to index.php
+    header("Location: index.php");
 endif
-// redirect to index.php
+
 
 ?>
